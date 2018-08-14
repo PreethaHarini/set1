f1afdf3fb21a2e8c440b966ddb93cebc8f3cfa72
PreethaHarini <42317155+PreethaHarini@users.noreply.github.com>
2018-08-14 22:33:30 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.lang.*;
+import java.util.Scanner;
+class oddeven
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a%2==0)
+{
+System.out.println("Even");
+}
+else
+{
+System.out.println("Odd");
+}
+}
+}
